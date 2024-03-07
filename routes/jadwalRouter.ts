5dfbe804d971e7c23cc44b78796535f0abcf0119
Haidar <16521522@mahasiswa.itb.ac.id>
2024-03-07 16:17:41 +0700

    feat: add api endpoint for listjadwal and landingpage

--- routes/jadwalRouter.ts
+++ routes/jadwalRouter.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import type { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-import { Jadwal } from "../types/jadwal";
+import { Jadwal, JadwalCatalog } from "../types/jadwal";
 
 export const jadwalRouter = express.Router();
 import * as jadwalService from "../controllers/jadwalController"
@@ -26,13 +26,45 @@ jadwalRouter.get("/viewJadwal/:jadwalId", async (request: Request, response: Res
     }
 });
 
-jadwalRouter.get("/landingPage/:page", async (request: Request, response: Response) => {
+jadwalRouter.get("/landingPage/event", async (request: Request, response: Response) => {
+    try {
+        let jadwal: Jadwal[];
+
+        jadwal = await jadwalService.getjadwal();
+        return response.status(200).json(jadwal);
+    }
+    catch (error: any) {
+        return response.status(500).json({ error: error.message });
+    }
+});
+
+
+jadwalRouter.get("/landingPage/catalog/:page", async (request: Request, response: Response) => {
     try {
         const { page } = request.params;
         const parsedPage = parseInt(page, 10);
+        let jadwal: JadwalCatalog[];
+
+        jadwal = await jadwalService.getCatalog(parsedPage);
+        return response.status(200).json(jadwal);
+    }
+    catch (error: any) {
+        return response.status(500).json({ error: error.message });
+    }
+});
+
+
+jadwalRouter.get("/listjadwal/:date", async (request: Request, response: Response) => {
+    try {
+        const { date } = request.params;
+        const searchDate = new Date(`${date}T00:00:00.000Z`);
+
+        searchDate.setHours(searchDate.getHours() - 7);
+
+        
         let jadwal: Jadwal[];
 
-        jadwal = await jadwalService.getjadwal(parsedPage);
+        jadwal = await jadwalService.getListJadwal(searchDate);
         return response.status(200).json(jadwal);
     }
     catch (error: any) {
