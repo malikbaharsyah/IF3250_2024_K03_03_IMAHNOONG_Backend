da99ce3f54bc7e7088929d5ddf3a6241e11ab435
Malik Baharsyah <13521029@mahasiswa.itb.ac.id>
2024-03-08 01:28:50 +0700

    feat: backend login

--- app.ts
+++ app.ts
@@ -3,8 +3,9 @@ import 'dotenv/config'
 
 import { catalogRouter } from './routes/catalogRouter';
 import { jadwalRouter } from './routes/jadwalRouter';
-
-
+import { registerAdminRouter } from './routes/registerAdminRouter';
+import { loginRouter, logoutRouter } from './routes/loginRouter';
+import { authRouter } from './routes/authRouter';
 
 const app = express();
 const port = process.env.PORT;
@@ -16,12 +17,15 @@ const corsOptions ={
    credentials:true,            
    optionSuccessStatus:200,
 }
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/catalog", catalogRouter);
 app.use("/api/jadwal", jadwalRouter);
-
+app.use("/api/registerAdmin", registerAdminRouter);
+app.use("/api/login", loginRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/logout", logoutRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello world!, api catalog is available');
