65630545a28fd4e8bcdc207d0bad1576cf9b8642
bintang433 <13521003@std.stei.itb.ac.id>
2024-03-07 13:45:35 +0700

    feat: pesanTiket

---
+++ controllers/pesanTiket.ts
@@ -0,0 +1,86 @@
+import { db } from "../utils/dbServer";
+import { Tiket } from "../types/tiket";
+import { error } from "console";
+
+export const pesanTiket = async (
+  namaPemesan: string,
+  jumlahTiket: number,
+  noTelpon: string,
+  email: string,
+  idJadwal: number
+): Promise<string> => {
+  try {
+    const newTiket = await db.tiket.create({
+      data: {
+        Nama_pemesan: namaPemesan,
+        jumlahTiket: jumlahTiket,
+        noTelepon: noTelpon,
+        email: email,
+        idJadwal: idJadwal,
+      },
+    });
+    return (
+      "Tiket untuk " + idJadwal + " berhasil dipesan dengan id: " + newTiket.id
+    );
+  } catch (error) {
+    console.log("Tiket gagal dipesan. " + error.code);
+    return "";
+  }
+};
+
+export const cekStok = async (
+  jumlahTiket: number,
+  idJadwal: number
+): Promise<string> => {
+  try {
+    const jadwalDipesan = await db.jadwal.findFirst({
+      select: {
+        kapasitas: true,
+      },
+      where: {
+        id: {
+          equals: idJadwal,
+        },
+      },
+    });
+    if (jadwalDipesan.kapasitas >= jumlahTiket) {
+      await db.jadwal.update({
+        where: {
+          id: idJadwal,
+        },
+        data: {
+          kapasitas: jadwalDipesan.kapasitas - jumlahTiket,
+        },
+      });
+      return "Stok berhasil dikurangi";
+    } else {
+      console.log("Kapasitas tidak cukup");
+      return "";
+    }
+  } catch (error) {
+    console.log("Tiket gagal dipesan. " + error.code);
+    return "";
+  }
+};
+
+export const revertStok = async (
+  jumlahTiket: number,
+  idJadwal: number
+): Promise<string> => {
+  try {
+    const res = await db.jadwal.update({
+      where: {
+        id: idJadwal,
+      },
+      data: {
+        kapasitas: {
+          increment: jumlahTiket,
+        },
+      },
+    });
+    return "Stok berhasil di kembalikan";
+  } catch (error) {
+    console.log("Stok gagal di kembalikan. " + error.code);
+    return "";
+  }
+};
---
+++ routes/pesanTiketRouter.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import type { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { Tiket } from "../types/tiket";
+
+import * as pesanTiketService from "../controllers/pesanTiket";
+import { catalogRouter } from "./catalogRouter";
+
+export const pesanTiketRouter = express.Router();
+
+catalogRouter.post("/", async (request: Request, response: Response) => {
+  try {
+    let result: Promise<string>;
+    let stokSufficient: Promise<string>;
+    stokSufficient = pesanTiketService.cekStok(
+      request.body.jumlahTiket,
+      request.body.idJadwal
+    );
+    if (stokSufficient) {
+      result = pesanTiketService.pesanTiket(
+        request.body.namaPemesan,
+        request.body.jumlahTiket,
+        request.body.noTelpon,
+        request.body.email,
+        request.body.idJadwal
+      );
+      return response.status(200).json(result);
+    } else {
+      return response.status(500).json("Stok Tidak Cukup");
+    }
+  } catch (error) {
+    return response.status(500).json(error.code);
+  }
+});
