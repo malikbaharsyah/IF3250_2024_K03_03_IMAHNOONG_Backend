e9fb2a7831eae94acb1eeb44b5b833a2f910db3b
Haidar <16521522@mahasiswa.itb.ac.id>
2024-03-07 16:19:40 +0700

    feat: add jadwalCatalog type

--- types/jadwal.ts
+++ types/jadwal.ts
@@ -5,6 +5,21 @@ export type Jadwal = {
     kapasitas: number;
     hargaTiket: number;
     planetariumId: number;
+    deskripsiJadwal: string; 
+}
+
+export type JadwalCatalog = {
+    id: number;
+    planetariumId: number;
+}
+
+export type jadwalDefault = {
+
+    id: number;
+    namaJadwal: string;
+    waktuKunjungan: string;
+    kapasitas: number;
+    hargaTiket: number;
+    planetariumId: number;
     deskripsiJadwal: string;
-    
 }
