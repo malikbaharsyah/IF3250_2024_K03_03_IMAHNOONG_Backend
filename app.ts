c4ca861d285e16d7fd1a547806a59c847999c210
Haidar <16521522@mahasiswa.itb.ac.id>
2024-03-24 23:42:36 +0700

    feat: add seeding script template and mail router

--- app.ts
+++ app.ts
@@ -8,6 +8,8 @@ import { registerAdminRouter } from './routes/registerAdminRouter';
 import { loginRouter, logoutRouter } from './routes/loginRouter';
 import { authRouter } from './routes/authRouter';
 import { pesanTiketRouter } from './routes/pesanTiketRouter';
+import { mailRouter } from './routes/mailRouter';
+import { insertJadwal } from "./utils/InsertData";
 
 const app = express();
 const port = process.env.PORT;
@@ -29,10 +31,18 @@ app.use("/api/login", loginRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/logout", logoutRouter);
 app.use("/api/pesanTiket", pesanTiketRouter);
+app.use("/api/email", mailRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello world!, api catalog is available');
 });
+
+// async function insertJadwalTenTimes() {
+//   for (let i = 0; i < 10; i++) {
+//       await insertJadwal();
+//   }
+// }
+// insertJadwalTenTimes();
 
 app.listen(port, () => {
   return console.log(`Express server is listening at http://localhost:${port} 🚀`);
