8d53316f81cff804c4f97127364bfa523af33f75
Muhammad Malik Ibrahim Baharsyah <13521029@mahasiswa.itb.ac.id>
2024-05-26 15:49:48 +0700

    feat: be for superadmin

--- routes/catalogRouter.ts
+++ routes/catalogRouter.ts
@@ -5,6 +5,7 @@ import { body, validationResult } from "express-validator";
 import { Planetarium } from "../types/planetarium";
 
 import * as catalogService from "../controllers/catalog";
+import { authToken, CustomRequest } from "../middlewares/auth";
 
 export const catalogRouter = express.Router();
 
@@ -13,6 +14,35 @@ catalogRouter.get("/", async (request: Request, response: Response) => {
     let catalog: Planetarium[];
     catalog = await catalogService.getCatalog();
     return response.status(200).json(catalog);
+  } catch (error: any) {
+    return response.status(500).json(error.message);
+  }
+});
+
+catalogRouter.post("/", authToken, async (request: Request, response: Response) => {
+  try {
+    const isSuperAdmin = (request as CustomRequest).isSuperAdmin;
+    if (!isSuperAdmin) {
+      return response.status(401).json("Unauthorized");
+    }
+    const data = {
+      namaPlanetarium: request.body.namaPlanetarium,
+      deskripsi: "",
+      prosedurPendaftaran: "",
+      tataTertib: "",
+      noTelepon: "",
+      imagePath: [],
+      lokasi: "",
+    }
+    const planetarium = await catalogService.createPlanetarium(
+      data.namaPlanetarium,
+      data.deskripsi,
+      data.prosedurPendaftaran,
+      data.tataTertib,
+      data.noTelepon,
+      data.imagePath,
+      data.lokasi)
+    return response.status(200).json(planetarium);
   } catch (error: any) {
     return response.status(500).json(error.message);
   }
