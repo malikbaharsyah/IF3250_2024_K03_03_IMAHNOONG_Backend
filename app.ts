3618eece6507064024e91a77ba1d827c62920d49
Muhammad Malik Ibrahim Baharsyah <13521029@mahasiswa.itb.ac.id>
2024-05-27 17:42:14 +0700

    feat: images

--- app.ts
+++ app.ts
@@ -32,6 +32,9 @@ const corsOptions ={
 }
 app.use(cors());
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(express.json());
 app.use("/api/catalog", catalogRouter);
 app.use("/api/jadwal", jadwalRouter);
@@ -47,7 +50,7 @@ app.use("/api/pesanan", pesananRouter);
 app.use("/api/dashboard", dashboardRouter);
 app.use("/api/jadwalDefault", jadwalDefaultRouter);
 app.use("/api/jadwalAdmin", jadwalAdminRouter);
-app.use("/api/adminnotifs", notifRouter);
+// app.use("/api/adminnotifs", notifRouter);
 // app.use("/api/review", reviewRouter);
 app.use("/api/assignAdmin", assignAdminRouter);
 
